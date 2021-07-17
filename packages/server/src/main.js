8c95e81c3fc54e544f68a31694a30316659723d9
Pedro Morais <meta3101@localiza.com>
2021-07-17 12:54:05 -0300

    first commit

---
+++ packages/server/src/main.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+
+const server = express();
+
+server.get("/status", (_, response) => {
+  response.send({
+    status: "Okay",
+  });
+});
+
+const enableCors = cors({ origin: "http://localhost:3000" });
+
+server
+  .options("/authenticate", enableCors)
+  .post("/authenticate", enableCors, express.json(), (request, response) => {
+    console.log("E-mail", request.body.email, "Senha", request.body.password);
+    response.send({
+      Okay: true,
+    });
+  });
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
+const HOSTNAME = process.env.HOSTNAME || "127.0.0.1";
+
+server.listen(PORT, HOSTNAME, () => {
+  console.log(`Server is listing at http://${HOSTNAME}:${PORT}`);
+});
