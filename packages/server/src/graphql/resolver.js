e73eb355b5f5f6e2dffa055ab86f93318a63a27d
Pedro Morais <pedro.s.morais@gmail.com>
2021-07-18 22:50:06 -0300

    graphql queries done

---
+++ packages/server/src/graphql/resolver.js
@@ -0,0 +1,13 @@
+import { resolvers as nodeResolvers } from "./Node/Node";
+import { resolvers as clientResolvers } from "./Client/Client";
+import { resolvers as demandResolvers } from "./Demand/Demand";
+const resolvers = {
+  ...nodeResolvers,
+  ...clientResolvers,
+  ...demandResolvers,
+  Query: {
+    ...clientResolvers.Query,
+    ...demandResolvers.Query,
+  },
+};
+export default resolvers;
--- packages/server/src/main.js
+++ packages/server/src/main.js
@@ -1,28 +1,26 @@
 import express from "express";
-import cors from "cors";
+import { ApolloServer, gql } from "apollo-server-express";
+import typeDefs from "./graphql/typeDefs";
+import resolvers from "./graphql/resolver";
 
-const server = express();
+const app = express();
 
-server.get("/status", (_, response) => {
-  response.send({
-    status: "Okay",
-  });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
 });
 
-const enableCors = cors({ origin: "http://localhost:3000" });
-
-server
-  .options("/authenticate", enableCors)
-  .post("/authenticate", enableCors, express.json(), (request, response) => {
-    console.log("E-mail", request.body.email, "Senha", request.body.password);
-    response.send({
-      Okay: true,
-    });
-  });
+server.applyMiddleware({
+  app,
+  cors: {
+    origin: "http://localhost:3000",
+  },
+  bodyParserConfig: true,
+});
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
 const HOSTNAME = process.env.HOSTNAME || "127.0.0.1";
 
-server.listen(PORT, HOSTNAME, () => {
+app.listen(PORT, HOSTNAME, () => {
   console.log(`Server is listing at http://${HOSTNAME}:${PORT}`);
 });
